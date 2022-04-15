64aa5b67f4b50fd2f1d130efbe85a6e71a3c3a06
kamal <kamal08111998@gmail.com>
2022-04-15 21:01:19 +0530

    Updated

--- April15/sum.java
+++ April15/sum.java
@@ -6,7 +6,9 @@ public class sum {
 
     public static void main(String[] args) {
 
+        System.out.print("Enter number a:");
         int a = scn.nextInt();
+        System.out.print("Enter number b:");
         int b = scn.nextInt();
         int c = a + b;
         System.out.println("sum of " + a + " and " + b + " is " + c);
